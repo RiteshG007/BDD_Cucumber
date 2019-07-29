53a39f1a3fa25ab02b62f57382253b8bc72aabd9
Ritesh <riteshgedam@gmail.com>
2019-07-29 15:56:01 +0530

    BDD

---
+++ src/test/java/Runner/TestRunner.java
@@ -0,0 +1,20 @@
+package Runner;
+
+import org.junit.runner.RunWith;
+import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
+
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/java/Features"
+		,glue= {"StepDefinitions"},
+				 //plugin = { "pretty", "html:target/cucumber-reports" },
+				 plugin = { "pretty", "json:target/cucumber.json" },
+				 //tags = {"@Smoke,@Sanity"},////or
+				 //tags = {"@Smoke","~@Sanity"},////And
+				 monochrome = true)
+public class TestRunner {
+
+}
